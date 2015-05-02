8439af04a73f77c6bfcc79082c2ed7a99fe2399b
Albin <binwei.zeng3@gmail.com>
2015-05-03 00:14:31 +0800

    44. Wildcard Matching

--- mysrc/wildcardMatching/wildcardMatching.cpp
+++ mysrc/wildcardMatching/wildcardMatching.cpp
@@ -27,7 +27,7 @@
 
 #include <iostream>
 #include <string>
-#include <stack>
+#include <vector>
 using namespace std;
 
 class Solution {
@@ -87,9 +87,74 @@ public:
 
       return result;
     }
+
+    bool isMatch2(string s, string p) {
+      bool result = false;
+
+      vector<vector<bool> > dp;
+
+      if (p.size()<1) return false;
+      // 第一行单独处理，作为后面的参考系
+      if (p[0] == '*') {
+        vector<bool> tmp;
+        for (int j=0; j<=s.size(); j++) {
+          tmp.push_back(true);
+        }
+        dp.push_back(tmp);
+      } else if (s.size()>0 && (p[0]=='?' || p[0]==s[0])){
+        vector<bool> tmp;
+        tmp.push_back(false);
+        tmp.push_back(true);
+        for (int j=2; j<=s.size(); j++) {
+          tmp.push_back(false);
+        }
+        dp.push_back(tmp);
+      } else {
+        vector<bool> tmp;
+        for (int j=0; j<=s.size(); j++) {
+          tmp.push_back(false);
+        }
+        dp.push_back(tmp);
+      }
+
+      for (int i=1; i<p.size(); i++) {
+        vector<bool> tmp;
+        if (p[i] == '*') {
+          for (int j=0; j<=s.size(); j++) {
+            if (dp[i-1][j] || j>0 && tmp[j-1]) {
+              tmp.push_back(true);
+            } else {
+              tmp.push_back(false);
+            }
+          }
+          dp.push_back(tmp);
+        } else if (p[i] == '?') {
+          for (int j=0; j<=s.size(); j++) {
+            if (j>0 && dp[i-1][j-1]) {
+              tmp.push_back(true);
+            } else {
+              tmp.push_back(false);
+            }
+          }
+          dp.push_back(tmp);
+        } else {
+          for (int j=0; j<=s.size(); j++) {
+            if (j>1 && dp[i-1][j-1] && p[i] == s[j-1]) {
+              tmp.push_back(true);
+            } else {
+              tmp.push_back(false);
+            }
+          }
+          dp.push_back(tmp);
+        }
+      }
+
+      return dp[p.size()-1][s.size()];
+    }
 };
 
 int main() {
+  cout<<"solution1: "<<endl;
   cout<<(new Solution)->isMatch("aa", "a")<<endl;
   cout<<(new Solution)->isMatch("aa", "aa")<<endl;
   cout<<(new Solution)->isMatch("aaa", "aa")<<endl;
@@ -100,4 +165,15 @@ int main() {
   cout<<(new Solution)->isMatch("", "*")<<endl;
   cout<<(new Solution)->isMatch("c", "*?*")<<endl;
   cout<<(new Solution)->isMatch("abefcdgiescdfimde", "ab*cd?i*de")<<endl;
+  cout<<"solution2: "<<endl;
+  cout<<(new Solution)->isMatch2("aa", "a")<<endl;
+  cout<<(new Solution)->isMatch2("aa", "aa")<<endl;
+  cout<<(new Solution)->isMatch2("aaa", "aa")<<endl;
+  cout<<(new Solution)->isMatch2("aa", "*")<<endl;
+  cout<<(new Solution)->isMatch2("aa", "a*")<<endl;
+  cout<<(new Solution)->isMatch2("ab", "?*")<<endl;
+  cout<<(new Solution)->isMatch2("aab", "c*a*b")<<endl;
+  cout<<(new Solution)->isMatch2("", "*")<<endl;
+  cout<<(new Solution)->isMatch2("c", "*?*")<<endl;
+  cout<<(new Solution)->isMatch2("abefcdgiescdfimde", "ab*cd?i*de")<<endl;
 }
